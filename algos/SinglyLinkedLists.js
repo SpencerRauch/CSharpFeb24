e53d10be93c14f9a9466d588fabfa4681ef77504
Spencer Rauch <rauchs@gmail.com>
2024-02-06 08:06:08 -0800

    mornin algos

--- algos/SinglyLinkedLists.js
+++ algos/SinglyLinkedLists.js
@@ -145,7 +145,7 @@ class SinglyLinkedList {
      * Removes the first node of this list.
      * - Time: (?).
      * - Space: (?).
-     * @returns {any} The data from the removed node.
+     * @returns {any} The data from the removed node or null if empty
      */
     removeHead() {
         //your code here
