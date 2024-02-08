cebc3de802d2bab62bd74fd591a4e6041462b85f
Spencer Rauch <rauchs@gmail.com>
2024-02-08 13:24:23 -0800

    custom validations

--- PetParty/Models/Pet.cs
+++ PetParty/Models/Pet.cs
@@ -6,12 +6,14 @@ namespace PetParty.Models;
 public class Pet
 {
     [Required]
+    [NoZNames]
     // [DataType(DataType.Password)]
     // [DataType(DataType.Date)] // do this if you don't want your datetime to track time
     public string Name { get;set; }
 
     [Display(Name = "Type of Pet")]
     [Required]
+    [ProvidedOptions("Dog","Fish","Cat","Zebra")]
     public string Species { get;set; }
 
     [Required(ErrorMessage = "Please supply age")]
@@ -21,3 +23,4 @@ public class Pet
     public bool IsCute { get;set; }
 
 }
+
---
+++ PetParty/Models/Validations.cs
@@ -0,0 +1,65 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PetParty.Models;
+
+public class NoZNamesAttribute : ValidationAttribute
+{    
+    // Call upon the protected IsValid method
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)    
+    {   
+        // We are expecting the value coming in to be a string
+        // so we need to do a bit of type casting to our object
+        // Strings work similarly to arrays under the hood 
+        // so we can grab the first letter using its index   
+        // If we discover that the first letter of our string is z...  
+        if (value == null || ((string)value).ToLower()[0] == 'z')
+        {        
+            // we return an error message in ValidationResult we want to render    
+            return new ValidationResult("No names that start with Z allowed!");   
+        } else {   
+            // Otherwise, we were successful and can report our success  
+            return ValidationResult.Success;  
+        }  
+    }
+}
+
+
+public class OptionsAttribute : ValidationAttribute
+{    
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)    
+    {   
+        string[] ValidOptions = {"Dog","Cat","Fish"}; //this should probably come from db
+
+        if (value == null ||!ValidOptions.Contains((string)value))
+        {        
+  
+            return new ValidationResult("Please choose Dog, Cat, or Fish");   
+        } else {   
+
+            return ValidationResult.Success;  
+        }  
+    }
+}
+
+public class ProvidedOptionsAttribute : ValidationAttribute
+{
+    public string[] Options { get;set; }
+
+    public ProvidedOptionsAttribute(params string[] options)
+    {
+        Options = options;
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)    
+    {   
+        if (value == null ||!Options.Contains((string)value))
+        {        
+  
+            return new ValidationResult("Please one of the supplied options");   
+        } else {   
+
+            return ValidationResult.Success;  
+        }  
+    }
+}
