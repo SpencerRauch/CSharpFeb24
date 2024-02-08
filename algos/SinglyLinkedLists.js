1393a81fc06ff1ff59990ca89aaf5bfeb59c71e8
Spencer Rauch <rauchs@gmail.com>
2024-02-08 07:28:30 -0800

    day 4 algos

--- algos/SinglyLinkedLists.js
+++ algos/SinglyLinkedLists.js
@@ -219,6 +219,30 @@ class SinglyLinkedList {
         return this.containsRecursive(val, current.next);
     }
 
+    /**
+     * Retrieves the data of the second to last node in this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {any} The data of the second to last node or null if there is no
+     *    second to last node.
+     */
+    secondToLast() {
+        //your code here
+    }
+
+    /**
+     * Removes the node that has the matching given val as it's data.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} val The value to compare to the node's data to find the
+     *    node to be removed.
+     * @returns {boolean} Indicates if a node was removed or not.
+     */
+    removeVal(val) {
+        //your code here
+    }
+
+
 }
 
 /******************************************************************* 
